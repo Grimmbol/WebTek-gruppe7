681bc7496616b7a26f81f317c59996150c8382b2
Grimmbol <krogstie.oystein@gmail.com>
2020-08-13 12:56:45 +0200

    oppdater tekst på "om oss"-sia, oppdater lenketekst i footer på hyrelenke

--- script/footer.js
+++ script/footer.js
@@ -6,7 +6,7 @@ document.write(`
    <div class="innhold">
     <ul class="left">
      <li><a href="#hjem">Til toppen</a></li>
-     <li><a href="index.html#booking">Book oss</a></li>
+     <li><a href="index.html#booking">Hyr oss</a></li>
     </ul>
     <ul class="right">
      <li><span id="sosialMedia"><a href="https://www.instagram.com/snaustrinda/" target="_blank"><img src="bilder/buttons/instagram.png" height="29" alt="Instagram" /></a><a href="https://www.facebook.com/Snaustrinda" target="_blank"><img src="bilder/buttons/facebook.png" height="29" alt="Facebook" /></a><a href="https://www.youtube.com/channel/UCR1IPmKx_pY15UJY5lYLEgA" target="_blank"><img src="bilder/buttons/youtube.png" height="29" alt="Youtube" /></a></span></li>
